43f6a912d0fd14a400276bec92c5a38edccda7ec
serpentineAutotheist <sharkattackzz@pm.me>
2026-06-12 14:00:49 -0400

    Create buffOverflow.c

---
+++ buffOverflow.c
@@ -0,0 +1,13 @@
+// gcc source.c -o vuln -no-pie -fno-stack-protector -z execstack -m32
+
+#include <stdio.h>
+
+void unsafe() {
+    char buffer[40];
+    puts("Overflow me");
+    gets(buffer);
+}
+
+void main() {
+    unsafe();
+}
